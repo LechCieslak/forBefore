462d325ba84fbfc5e48711d4bb881d23a9af9c96
LechCieslak <cieslak.lech@gmail.com>
2020-01-28 15:30:13 +0100

    js function

--- app.js
+++ app.js
@@ -1,2 +1,19 @@
 console.log("Hello");
 console.log(2+2);
+
+function print (a,b) {
+    var a=1;
+    var b=2;
+    console.log(a+b);
+    
+}
+
+function changeH() {
+    var bckC = document.getElementById("my");
+    bckC.style.height = "200px";
+}
+
+function changeW() {
+    var bckC2 = document.getElementById("my2");
+    bckC2.style.width = "200px";
+}
