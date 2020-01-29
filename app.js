86459c110f20f7cbe950734c8e12accdb7766d4e
LechCieslak <cieslak.lech@gmail.com>
2020-01-29 16:15:59 +0100

    change background by class

--- app.js
+++ app.js
@@ -1,5 +1,4 @@
 console.log("Hello");
-console.log(2+2);
 
 function print (a,b) {
     var a=1;
@@ -11,9 +10,19 @@ function print (a,b) {
 function changeH() {
     var bckC = document.getElementById("my");
     bckC.style.height = "200px";
+    console.log("dziala changeH");
+    
 }
 
 function changeW() {
     var bckC2 = document.getElementById("my2");
     bckC2.style.width = "200px";
+    console.log("dziala changeW");
+}
+
+function changeByClass() {
+    var a = document.getElementsByClassName("second")[0];
+    a.style.backgroundColor = "teal";
+    console.log("dzialaClass");
+    
 }
